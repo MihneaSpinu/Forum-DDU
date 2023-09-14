1c67c912246a95c02dd14ae5add3cf6d3f1e441d
jepp8530 <jepp8530@mail.hansenberg.dk>
2023-09-14 09:01:48 +0100

    Home setup

--- app/frontend/pages/home.php
+++ app/frontend/pages/home.php
@@ -1,15 +1,17 @@
 <div class="container" style="margin-top:30px">
     <div class="row">
       <div class="col-sm-4">
-        <h2>About Me</h2>
-        <h5>Photo of me:</h5>
-        <div class="fakeimg">Fake Image</div>
-        <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-        <h3>Some Links</h3>
-        <p>Lorem ipsum dolor sit ame.</p>
+        <h2>About gooning</h2>
+        <h5>Photo of gooning:</h5>
+        <div class="fakeimg">Real Image</div>
+        <p>"Gooning" may be most simply defined as that state usually achieved after a prolonged edging session, when a man becomes completely hypnotized by the feeling radiating his penis. Since a gooning state can only be achieved after edging, the man's dick will have become mightily aroused at this point and every caress the male genitals are subjected to will trigger potent elation. As the man keeps edging and thus keeps experiencing intense pleasure, he enters a state of trance where his mind intimately merges with his cock : the gooning state, where he and his dick become one.<br><br>
+        To be even more accurate, when the gooning state is achieved, the man's body becomes for all intents and purposes an appendage to his erection. When this state is achieved, the male becomes freed of all social codes of conduct, and his arousal, alone, dictates his reactions. As a result, a gooned out man will become very expressive and demonstrative : he may become very vocal, while his body and face might take on undignified expressions and poses, all in response to the intensely exquisite caresses his penis is exposed to. Hence the term "goon", since at this point the man effectively looks like a silly, foolish, or eccentric person.<br><br>
+        Gooning is not a narcissistic manifestation. Narcissism is extreme selfishness, with a grandiose view of one's own talents. Rather, gooning is closer to a meditation experience, where the mind and the body align, focused on a single thought (or feeling, in this case).</p>
+        <h3>Some Links to Gooning</h3>
+        <p>Goon goon goon gooning.</p>
         <ul class="nav nav-pills flex-column">
           <li class="nav-item">
-            <a class="nav-link active" href="#">Active</a>
+            <a class="nav-link active" href="https://uapress.arizona.edu/book/them-goon-rules">Goon rules</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
@@ -24,17 +26,20 @@
         <hr class="d-sm-none">
       </div>
       <div class="col-sm-8">
-        <h2>TITLE HEADING</h2>
-        <h5>Title description, Dec 7, 2017</h5>
-        <div class="fakeimg">Fake Image</div>
-        <p>Some text..</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        <h2>Goon News!!!</h2>
+        <h5>Gunnar Johannsson gooning, 13/09/2023</h5>
+        <div class="fakeimg">Real Image</div>
+        <p>Some text about gooning</p>
+        <p>You have to master edging first. Stroking until you reach the closest point to cumming then you stop or slowly stroke to lighten up the intense pleasure that made u almost cum. You then work up again to the edge and repeat. After you keep reaching this edge over n overn over you enter a trance like gooning state. You are at a heightened level of arousal. You reach a high level of pleasure and never let it go below that level. You stay at that level and keep pushing a little higher to the edge and come back a bit to not cum and repeat. Some training would be to have ruined orgasms and then keep jerking after and to try and produce as much pre cum as you can during your jerk session. Don’t just glide your hand up n down when u jerk but work that dick. You should be wincing not to cum the whole time..</p>
         <br>
         <h2>TITLE HEADING</h2>
         <h5>Title description, Sep 2, 2017</h5>
         <div class="fakeimg">Fake Image</div>
-        <p>Some text..</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        <p>Some about gooning..</p>
+        <p>"Gooning" may be most simpwy defined as that state usuawwy achieved aftew a pwowonged edging session, when a man becomes compwetewy hypnotized by the feewing wadiating his penis. Since a gooning state can onwy be achieved aftew edging, the man's dick wiww have become mightiwy awoused at this point and evewy cawess the mawe genitaws awe subjected to wiww twiggew potent ewation. As the man keeps edging and thus keeps expewiencing intense pweasuwe, he entews a state of twance whewe his mind intimatewy mewges with his cock : the gooning state, whewe he and his dick become one.<br><br>
+        To be even mowe accuwate, when the gooning state is achieved, the man's body becomes fow aww intents and puwposes an appendage to his ewection. When this state is achieved, the mawe becomes fweed of aww sociaw codes of conduct, and his awousaw, awone, dictates his weactions. As a wesuwt, a gooned out man wiww become vewy expwessive and demonstwative : he may become vewy vocaw, whiwe his body and face might take on undignified expwessions and poses, aww in wesponse to the intensewy exquisite cawesses his penis is exposed to. Hence the tewm "goon", since at this point the man effectivewy wooks wike a siwwy, foowish, ow eccentwic pewson.
+
+Gooning is not a nawcissistic manifestation. Nawcissism is extweme sewfishness, with a gwandiose view of one's own tawents. Wathew, gooning is cwosew to a meditation expewience, whewe the mind and the body awign, focused on a singwe thought (ow feewing, in this case)..</p>
       </div>
     </div>
   </div>
