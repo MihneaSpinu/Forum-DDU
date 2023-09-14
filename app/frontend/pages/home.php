443ae2d596fb2deddb363baad9b085daff775c07
jepp8530 <jepp8530@mail.hansenberg.dk>
2023-09-14 09:17:14 +0100

    Wip

--- app/frontend/pages/home.php
+++ app/frontend/pages/home.php
@@ -32,9 +32,9 @@
         <p>Some text about gooning</p>
         <p>You have to master edging first. Stroking until you reach the closest point to cumming then you stop or slowly stroke to lighten up the intense pleasure that made u almost cum. You then work up again to the edge and repeat. After you keep reaching this edge over n overn over you enter a trance like gooning state. You are at a heightened level of arousal. You reach a high level of pleasure and never let it go below that level. You stay at that level and keep pushing a little higher to the edge and come back a bit to not cum and repeat. Some training would be to have ruined orgasms and then keep jerking after and to try and produce as much pre cum as you can during your jerk session. Don’t just glide your hand up n down when u jerk but work that dick. You should be wincing not to cum the whole time..</p>
         <br>
-        <h2>TITLE HEADING</h2>
-        <h5>Title description, Sep 2, 2017</h5>
-        <div class="fakeimg">Fake Image</div>
+        <h2>2 Million Goon Members</h2>
+        <h5>This forum has oficcially reached 2 million users, 10/7/2023</h5>
+        <div class="fakeimg">Real Image</div>
         <p>Some about gooning..</p>
         <p>"Gooning" may be most simpwy defined as that state usuawwy achieved aftew a pwowonged edging session, when a man becomes compwetewy hypnotized by the feewing wadiating his penis. Since a gooning state can onwy be achieved aftew edging, the man's dick wiww have become mightiwy awoused at this point and evewy cawess the mawe genitaws awe subjected to wiww twiggew potent ewation. As the man keeps edging and thus keeps expewiencing intense pweasuwe, he entews a state of twance whewe his mind intimatewy mewges with his cock : the gooning state, whewe he and his dick become one.<br><br>
         To be even mowe accuwate, when the gooning state is achieved, the man's body becomes fow aww intents and puwposes an appendage to his ewection. When this state is achieved, the mawe becomes fweed of aww sociaw codes of conduct, and his awousaw, awone, dictates his weactions. As a wesuwt, a gooned out man wiww become vewy expwessive and demonstwative : he may become vewy vocaw, whiwe his body and face might take on undignified expwessions and poses, aww in wesponse to the intensewy exquisite cawesses his penis is exposed to. Hence the tewm "goon", since at this point the man effectivewy wooks wike a siwwy, foowish, ow eccentwic pewson.
