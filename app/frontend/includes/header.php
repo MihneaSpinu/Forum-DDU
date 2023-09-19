50d18326ba6f80d778adeb29cda497a91ae8f558
Gunnar <gunnarmani04@gmail.com>
2023-09-19 13:05:50 +0200

    header og navbar ting

--- app/frontend/includes/header.php
+++ app/frontend/includes/header.php
@@ -6,6 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo FRONTEND_ASSET . 'css/style.css'; ?>">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
@@ -17,47 +18,41 @@
                                 ?>">
 -->
   <style>
-    .fakeimg {
-      height: 200px;
-      background: #aaa;
-    }
-    body {
-      background-color: #fffff2;
-    }
+
   </style>
 </head>
 
 <body>
-  <!-- round logo -->
-  <div class="jumbotron text-center" style="margin-bottom:0; background-image: url('https://mdbootstrap.com/img/Photos/Others/images/76.jpg'); ">
-      <img src="https://mdbootstrap.com/img/logo/mdb-transparent.png" class="rounded" alt="Cinque Terre" height="100" align="left">
+  <div align="right" class="jumbotron header">
+    <!-- Placeholder -->
+    <img src="https://mdbootstrap.com/img/logo/mdb-transparent.png" class="rounded" alt="Cinque Terre" height="100" align="left">
 
-      <?php if ($user->isLoggedIn()) : ?>
-        <h3 align="right">Hello, <?php echo $user->data()->name; ?></h3>
-        <ul align="right" class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="profile.php">
-              <span class="glyphicon glyphicon-user"></span> Profile
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="logout.php">
-              <span class="glyphicon glyphicon-log-out"></span> Logout
-            </a>
-          </li>
-        </ul>
-      <?php else : ?>
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="register.php">
-              <span class="glyphicon glyphicon-user"></span> Register
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="login.php">
-              <span class="glyphicon glyphicon-log-in"></span> Log-in
-            </a>
-          </li>
-        </ul>
-      <?php endif; ?>
-    </div>
+    <?php if ($user->isLoggedIn()) : ?>
+      <h3 >Hello, <?php echo $user->data()->name; ?></h3>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item profile">
+          <a class="nav-link" href="profile.php">
+            <span class="glyphicon glyphicon-user"></span> Profile
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">
+            <span class="glyphicon glyphicon-log-out"></span> Logout
+          </a>
+        </li>
+      </ul>
+    <?php else : ?>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">
+            <span class="glyphicon glyphicon-user"></span> Register
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">
+            <span class="glyphicon glyphicon-log-in"></span> Log-in
+          </a>
+        </li>
+      </ul>
+    <?php endif; ?>
+  </div>
--- app/frontend/includes/navbar.php
+++ app/frontend/includes/navbar.php
@@ -1,25 +1,53 @@
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="forum.php">Forum</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="blogs.php">Blogs</a>
-      <li class="nav-item" style="position: absolute; right:600px">
-        <form class="form-inline" action="search.php" method="GET">
-          <input class="form-control mr-sm-2" type="text" name="search" placeholder="Search">
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <form class="form-inline mx-auto my-2 my-lg-0 d-lg-none" action="search.php" method="GET">
+      <div class="input-group">
+        <input class="form-control" type="text" name="search" placeholder="Search">
+        <div class="input-group-append">
           <button class="btn btn-success" type="submit">Search</button>
-        </form>
-      <li class="nav-item" style="position:absolute; right: 15px">
-        <a class="nav-link" href="webshop.php">Webshop</a>
-      </li>
-    </ul>
+        </div>
+      </div>
+    </form>
+
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="forum.php">Forums</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="forum.php">Forum1</a>
+            <a class="dropdown-item" href="forum.php">Forum2</a>
+            <a class="dropdown-item" href="forum.php">Forum3</a>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="blogs.php">Blogs</a>
+        </li>
+        <li class="nav-item d-lg-none">
+          <a class="nav-link webshop" href="webshop.php">Webshop</a>
+        </li>
+      </ul>
+
+      <form class="form-inline mx-auto d-lg-block d-none" action="search.php" method="GET">
+        <div class="input-group">
+          <input class="form-control" type="text" name="search" placeholder="Search">
+          <div class="input-group-append">
+            <button class="btn btn-success" type="submit">Search</button>
+          </div>
+        </div>
+      </form>
+      <ul class="navbar-nav d-none d-lg-block ml-auto">
+        <li class="nav-item">
+          <a class="nav-link webshop" href="webshop.php">Webshop</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
