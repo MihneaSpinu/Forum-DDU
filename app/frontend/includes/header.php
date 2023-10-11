916c49a5e0086c7ba90458e5af217d22848d1859
Hamsterbil <68914913+Hamsterbil@users.noreply.github.com>
2023-10-11 02:20:55 +0200

    header & navbar responsive fix done now yes thank you finally

--- app/frontend/includes/header.php
+++ app/frontend/includes/header.php
@@ -15,32 +15,32 @@
 </head>
 
 <body>
-  <div class="jumbotron header py-3 px-3">
-    <div class="container-fluid row">
-      <a href="index.php" class="mr-auto col-md-2">
-        <img src="<?php echo FRONTEND_ASSET . 'Img/LogoTitle.png'; ?>" type="image/png" width="100%">
+  <!-- <div class="jumbotron header py-3 px-3"> -->
+  <header class="d-flex align-items-center justify-content-center justify-content-md-between py-3">
+    <div class="col-md-auto ml-5">
+      <a href="index.php" class="d-inline-flex link-body-emphasis text-decoration-none">
+        <img class="bi" src="<?php echo FRONTEND_ASSET . 'Img/LogoTitle.png'; ?>" type="image/png" height="150" role="img">
       </a>
+    </div>
 
-      <div class="col-md-2 ml-auto">
-        <div class="mx-auto my-auto"> <?php if ($user->isLoggedIn()) : ?> <h3 style="color: rgba(255,255,255,.5);">Hello, <?php echo $user->data()->name; ?></h3>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item profile">
-              <a class="nav-link" href="profile.php">
-                <span class="glyphicon glyphicon-user"></span> Profile
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="logout.php">
-                <span class="glyphicon glyphicon-log-out"></span> Logout
-              </a>
-            </li>
-          </ul>
-        <?php else : ?>
-
-          <a type="button" class="btn btn-outline-primary me-2" href="login.php">Login</a>
-          <a type="button" class="btn btn-primary" href="register.php">Sign-up</a>
+    <div class="col-md-auto mr-5">
+        <div class="my-auto text-right"> <?php if ($user->isLoggedIn()) : ?> <h3 class="ml-auto" style="color: white;">Hello, <?php echo $user->data()->name; ?></h3>
+            <ul class="navbar-nav">
+              <li class="nav-item profile">
+                <a class="nav-link" href="profile.php">
+                  <span class="glyphicon glyphicon-user"></span> Profile
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="logout.php">
+                  <span class="glyphicon glyphicon-log-out"></span> Logout
+                </a>
+              </li>
+            </ul>
+          <?php else : ?>
+            <a type="button" class="btn btn-outline-primary me-2" href="login.php">Login</a>
+            <a type="button" class="btn btn-primary" href="register.php">Sign-up</a>
+          <?php endif; ?>
         </div>
       </div>
-    <?php endif; ?>
-    </div>
-  </div>
+  </header>
--- app/frontend/includes/navbar.php
+++ app/frontend/includes/navbar.php
@@ -1,62 +1,14 @@
-<nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top ">
-  <div class="container">
-    <div class="row">
-      <button class="navbar-toggler col" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <form class="form-inline mx-auto my-2 my-lg-0 d-lg-none col" action="search.php" method="GET">
-        <div class="input-group">
-          <input class="form-control" type="text" name="search" placeholder="Search">
-        </div>
-      </form>
-    </div>
-
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-      <div class="row w-75 mx-auto">
-        <ul class="navbar-nav col">
-          <li class="nav-item">
-            <a class="nav-link" href="index.php">Home</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="forum.php">Forums</a>
-            <div class="dropdown-menu bg-dark">
-              <a class="dropdown-item" href="forum.php">Finite Abys</a>
-              <a class="dropdown-item" href="forum.php">Mod Workshop</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="blogs.php">Blogs</a>
-          </li>
-          <li class="nav-item d-lg-none">
-            <a class="nav-link" href="webshop.php">Webshop</a>
-          </li>
-        </ul>
-
-        <form class="form-inline mx-auto d-lg-block d-none col" action="search.php" method="GET">
-          <div class="input-group">
-            <input class="form-control" type="text" name="search" placeholder="Search">
-          </div>
-        </form>
-        <ul class="navbar-nav d-none d-lg-block col">
-          <li class="nav-item">
-            <a class="nav-link" href="webshop.php">Webshop</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-</nav>
-
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="#">Navbar</a>
-
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+  <form class="form-inline my-lg-0 d-lg-none mx-auto">
+      <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+  </form>
+  <div class="collapse navbar-collapse justify-content-center mx-5" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mt-2 mr-auto mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Home</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="forum.php">Forums</a>
@@ -69,8 +21,8 @@
         <a class="nav-link" href="blogs.php">Blogs</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <form class="position-absolute form-inline my-2 my-lg-0 d-none d-lg-block">
+      <input class="form-control" type="search" placeholder="Search" aria-label="Search">
     </form>
     <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
       <li class="nav-item">
