df404301091370b2bfde3481db70d866a1155ed4
jepp8530 <jepp8530@mail.hansenberg.dk>
2023-11-02 10:52:31 +0100

    hotfix

--- app/frontend/pages/posts.php
+++ app/frontend/pages/posts.php
@@ -2,37 +2,11 @@
     <div class="row">
         <div class="jumbotron text-center w-100 bg-dark" style="margin-bottom:0">
             <?php
-<<<<<<< HEAD
             echo '<h3 class="border-bottom">' . $topic->name . '</h3>';
             echo '<h1 class="">' . $post->title . '</h1>';
             echo '<p class="lead ">Posted by: ' . $creator->username . '</p>';
             echo '<p class="lead ">Posted on: ' . $post->created_at . '</p>';
             echo '<p class="lead ">' . $post->content . '</p>';
-=======
-<<<<<<< Updated upstream
-            echo '<h1 class="text-dark">' . $post->title . '</h1>';
-            echo '<p class="lead text-dark">Posted by: ' . $creator->username . '</p>';
-            echo '<p class="lead text-dark">Posted on: ' . $post->created_at . '</p>';
-            echo '<p class="lead text-dark">' . $post->content . '</p>';
-=======
-            echo '<a href="create-post.php?channel_id=' . $topic_id . '" class="btn btn-primary">Create Post</a>';
-
-            if ($posts->count()) {
-                foreach ($posts->results() as $p) {
-                    echo '<div class="card">';
-                    echo '<div class="card-body">';
-                    echo '<h4 class="card-title">' . $p->title . '</h4>';
-                    echo '<p class="card-text">' . $p->content . '</p>';
-                    echo '<a href="comments.php?post_id=' . $p->post_id . '" class="btn btn-primary">View Post</a>';
-                    echo '</div>';
-                    echo '</div>';
-                    
-                }
-            } else {
-                echo '<div class="alert alert-danger"><strong></strong>No posts found!</div>';
-            }
->>>>>>> Stashed changes
->>>>>>> development
             ?>
         </div>
     </div>
